448e568f52f64b09d5b53054ad5fd57c0c49e6dd
KalashShah19 <92210070+KalashShah19@users.noreply.github.com>
2025-01-24 09:13:56 +0530

    Updated English Playlist
    Added I Wanna Be Yours song

--- scripts/english.js
+++ scripts/english.js
@@ -1,6 +1,7 @@
 const songs = [
     { title: "Sunflower", url: "./songs/Sunflower.mp3" },
     { title: "Die With A Smile", url: "./songs/Die With A Smile.mp3" },
+    { title: "I Wanna Be Yours", url: "./songs/I Wanna Be Yours.m4a" },
     { title: "Until I Found You", url: "./songs/Until I Found You.mp3" },
     { title: "In The Name Of Love", url: "./songs/In The Name Of Love.mp3" },
 ];
